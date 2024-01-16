41e821e62d046a87cc038289f496b6aca6d9f95c
Ahmedelhelw <elhelwahmed4@gmail.com>
2024-01-16 17:09:32 +0200

    fix Step component loading condition

--- components/Step.jsx
+++ components/Step.jsx
@@ -3,7 +3,7 @@ const Step = ({ title, info, loading, color }) => {
   return (
     <div className="last-update">
       <span className="text-slate-400">{title}</span>
-      {true ? (
+      {loading ? (
         <div className="w-52 animate-pulse  h-3 rounded-md mt-2 mx-auto bg-slate-200 " />
       ) : (
         <h3 style={{ color }} className={`font-bold `}>
