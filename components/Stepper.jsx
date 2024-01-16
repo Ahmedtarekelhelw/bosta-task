06e48c026d8e5c23ebe0434ec6bd88a538b5d4d9
Ahmedelhelw <elhelwahmed4@gmail.com>
2024-01-16 20:24:02 +0200

    set initial value in both first load  && with no data && refactor to the code

--- components/Stepper.jsx
+++ components/Stepper.jsx
@@ -1,5 +1,51 @@
-const Stepper = () => {
-  return <div></div>;
+"use client";
+
+import { FaPersonCircleCheck, FaTruck } from "react-icons/fa6";
+import Progress from "./Progress";
+import { TfiTruck } from "react-icons/tfi";
+import useDirection from "@/hooks/useDirection";
+
+const Stepper = ({ completed, color, StepsText }) => {
+  const { reverseFlex, dir } = useDirection();
+
+  return (
+    <div
+      className={`shipment-progress md:flex p-5 pb-16  ${reverseFlex} hidden`}
+    >
+      <Progress
+        index={1}
+        completed={completed}
+        color={color}
+        text={StepsText[0]}
+      />
+      <Progress
+        index={2}
+        middle
+        style={dir === "left" ? "left-[60%]" : "right-[60%]"}
+        completed={completed}
+        color={color}
+        text={StepsText[1]}
+      />
+      <Progress
+        index={3}
+        middle
+        style={dir === "left" ? "right-[60%]" : "left-[60%]"}
+        completed={completed}
+        Icon={dir === "right" ? <TfiTruck size={20} /> : <FaTruck size={20} />}
+        dir={dir}
+        color={color}
+        text={StepsText[2]}
+      />
+      <Progress
+        index={4}
+        dir={dir}
+        completed={completed}
+        Icon={<FaPersonCircleCheck size={20} />}
+        color={color}
+        text={StepsText[3]}
+      />
+    </div>
+  );
 };
 
 export default Stepper;
--- components/Tracking.jsx
+++ components/Tracking.jsx
@@ -1,17 +1,12 @@
 "use client";
 
-import useFetch from "@/hooks/useFetch";
 import moment from "moment";
 import Step from "./Step";
 import useSearch from "@/hooks/useSearch";
 import useDirection from "@/hooks/useDirection";
-import Progress from "./Progress";
-
-import { TfiTruck } from "react-icons/tfi";
-import { FaTruck } from "react-icons/fa";
-import { FaPersonCircleCheck } from "react-icons/fa6";
 import { useTrackingData } from "@/context/TrackingDataContext";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
+import Stepper from "./Stepper";
 
 const Tracking = ({ StepTitle, StepStatus, StepsText }) => {
   const { track_num } = useSearch();
@@ -30,28 +25,37 @@ const Tracking = ({ StepTitle, StepStatus, StepsText }) => {
 
   const completed = data?.CurrentStatus?.state === "DELIVERED" ? true : false;
 
-  const StepInfo = [
-    {
-      title: `${StepTitle[0]}. ${track_num}`,
-      info: StepStatus[data?.CurrentStatus?.state],
-      color: statusColor[data?.CurrentStatus?.state],
-    },
-    {
-      title: StepTitle[1],
-      info: moment(data.CurrentStatus?.timestamp).utc().local().format("llll"),
-    },
-    {
-      title: StepTitle[2],
-      info: data.provider,
-    },
-    {
-      title: StepTitle[3],
-      info: moment(data.PromisedDate).utc().local().format("YYYY-MM-DD"),
-    },
-  ];
+  const StepInfo = useMemo(
+    () => [
+      {
+        title: `${StepTitle[0]}. ${track_num}`,
+        info: StepStatus[data?.CurrentStatus?.state] || "N/A",
+        color: statusColor[data?.CurrentStatus?.state],
+      },
+      {
+        title: StepTitle[1],
+        info: data.CurrentStatus?.timestamp
+          ? moment(data.CurrentStatus?.timestamp).utc().local().format("llll")
+          : "N/A",
+      },
+      {
+        title: StepTitle[2],
+        info: data.provider || "N/A",
+      },
+      {
+        title: StepTitle[3],
+        info: data.PromisedDate
+          ? moment(data.PromisedDate).utc().local().format("YYYY-MM-DD")
+          : "N/A",
+      },
+    ],
+    [data]
+  );
+
   return (
     <div className="border rounded-md  mt-10  flex  md:flex-col">
       {/* shipment-info */}
+
       <div
         className={`shipment-info border-b mb-5 p-5 md:text-${dir} flex flex-col gap-5 md:gap-0 md:flex-row md:items-center w-full md:w-auto text-center  ${reverseFlex} justify-between `}
       >
@@ -67,44 +71,11 @@ const Tracking = ({ StepTitle, StepStatus, StepsText }) => {
       </div>
 
       {/* shipment-progress  */}
-      <div
-        className={`shipment-progress md:flex p-5 pb-16  ${reverseFlex} hidden`}
-      >
-        <Progress
-          index={1}
-          completed={completed}
-          color={statusColor[data?.CurrentStatus?.state]}
-          text={StepsText[0]}
-        />
-        <Progress
-          index={2}
-          middle
-          style={dir === "left" ? "left-[60%]" : "right-[60%]"}
-          completed={completed}
-          color={statusColor[data?.CurrentStatus?.state]}
-          text={StepsText[1]}
-        />
-        <Progress
-          index={3}
-          middle
-          style={dir === "left" ? "right-[60%]" : "left-[60%]"}
-          completed={completed}
-          Icon={
-            dir === "right" ? <TfiTruck size={20} /> : <FaTruck size={20} />
-          }
-          dir={dir}
-          color={statusColor[data?.CurrentStatus?.state]}
-          text={StepsText[2]}
-        />
-        <Progress
-          index={4}
-          dir={dir}
-          completed={completed}
-          Icon={<FaPersonCircleCheck size={20} />}
-          color={statusColor[data?.CurrentStatus?.state]}
-          text={StepsText[3]}
-        />
-      </div>
+      <Stepper
+        completed={completed}
+        color={statusColor[data?.CurrentStatus?.state]}
+        StepsText={StepsText}
+      />
     </div>
   );
 };
