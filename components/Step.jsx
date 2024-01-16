69a245af285ee84c000e201da6a5e68a20df8eb7
Ahmedelhelw <elhelwahmed4@gmail.com>
2024-01-16 17:08:39 +0200

    Refactor loading indicator position for Step component

--- components/Step.jsx
+++ components/Step.jsx
@@ -3,8 +3,8 @@ const Step = ({ title, info, loading, color }) => {
   return (
     <div className="last-update">
       <span className="text-slate-400">{title}</span>
-      {loading ? (
-        <div className="w-52 animate-pulse  h-3 rounded-md mt-2 bg-slate-200 " />
+      {true ? (
+        <div className="w-52 animate-pulse  h-3 rounded-md mt-2 mx-auto bg-slate-200 " />
       ) : (
         <h3 style={{ color }} className={`font-bold `}>
           {info}
