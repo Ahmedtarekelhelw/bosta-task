f97970a53ebfe2c0611b913c6e692f9ee514966e
Ahmedelhelw <elhelwahmed4@gmail.com>
2024-01-16 20:26:25 +0200

    seprate all translation of the home to be more readable

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,54 +1,28 @@
+import { useMemo } from "react";
+import { useTranslations } from "next-intl";
+
 import Tracking from "@/components/Tracking";
 import TrackingData from "@/components/TrackingData";
-import { useTranslations } from "next-intl";
-import { useMemo } from "react";
+
+import {
+  stepStatus,
+  stepsText,
+  stepTitle,
+  tableHeaders,
+  trackingData,
+  Translate,
+} from "@/helper/helper";
 
 export default function Home() {
   const t = useTranslations("step");
   const t2 = useTranslations("tracking_info");
 
-  const StepTitle = useMemo(
-    () => [
-      t("shipment_No"),
-      t("last_update"),
-      t("merchant_name"),
-      t("delivering_at"),
-    ],
-    [t]
-  );
+  const StepTitle = useMemo(() => Translate(t, stepTitle), [t]);
+  const StepStatus = useMemo(() => Translate(t, stepStatus), [t]);
+  const StepsText = useMemo(() => Translate(t, stepsText), [t]);
 
-  const StepStatus = useMemo(
-    () => ({
-      DELIVERED: t("DELIVERED"),
-      CANCELLED: t("CANCELLED"),
-      DELIVERED_TO_SENDER: t("DELIVERED_TO_SENDER"),
-    }),
-    [t]
-  );
-
-  const StepsText = useMemo(
-    () => [
-      t("sh_created"),
-      t("sh_received_from_merchant"),
-      t("sh_on_the_way"),
-      t("sh_delivered"),
-    ],
-    [t]
-  );
-  const trackingData = useMemo(
-    () => [
-      t2("Delivery_address"),
-      t2("shipment_problem"),
-      t2("Report_a_problem"),
-      t2("Shipment_details"),
-    ],
-    [t2]
-  );
-
-  const tableHeaders = useMemo(
-    () => [t2("hub"), t2("date"), t2("time"), t2("state")],
-    [t2]
-  );
+  const Trackingdata = useMemo(() => Translate(t2, trackingData), [t2]);
+  const TableHeaders = useMemo(() => Translate(t2, tableHeaders), [t2]);
 
   return (
     <div className="mx-10 mb-36">
@@ -57,7 +31,7 @@ export default function Home() {
         StepStatus={StepStatus}
         StepsText={StepsText}
       />
-      <TrackingData trackingData={trackingData} tableHeaders={tableHeaders} />
+      <TrackingData trackingData={Trackingdata} tableHeaders={TableHeaders} />
     </div>
   );
 }
---
+++ helper/helper.js
@@ -0,0 +1,38 @@
+export const Translate = (translator, data) => {
+  if (Array.isArray(data)) return data.map((key) => translator(key));
+  const translatedObj = {};
+  Object.entries(data).forEach(([key, value]) => {
+    translatedObj[key] = translator[value];
+  });
+
+  return translatedObj;
+};
+
+export const stepTitle = [
+  "shipment_No",
+  "last_update",
+  "merchant_name",
+  "delivering_at",
+];
+
+export const stepStatus = {
+  DELIVERED: "DELIVERED",
+  CANCELLED: "CANCELLED",
+  DELIVERED_TO_SENDER: "DELIVERED_TO_SENDER",
+};
+
+export const stepsText = [
+  "sh_created",
+  "sh_received_from_merchant",
+  "sh_on_the_way",
+  "sh_delivered",
+];
+
+export const trackingData = [
+  "Delivery_address",
+  "shipment_problem",
+  "Report_a_problem",
+  "Shipment_details",
+];
+
+export const tableHeaders = ["hub", "date", "time", "state"];
